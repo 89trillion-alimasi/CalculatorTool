7957b2bd4a867ca3a426c73c5a36feb212b334dd
89trillion-alimasi <alimasi@89trillion.com>
2021-07-07 22:59:37 +0800

    重新定义了结构体和实现了第二种方法

--- ctr/controller.go
+++ ctr/controller.go
@@ -2,6 +2,7 @@ package ctr
 
 import (
 	"CalculatorTool/service"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"regexp"
 	"strings"
@@ -12,31 +13,47 @@ func Controller(c *gin.Context) {
 	c.Request.URL.RawQuery = strings.ReplaceAll(c.Request.URL.RawQuery, "+", "%2b")
 	//expression传进来的表达式
 	expression := c.Query("expr")
-	//fmt.Println(expression)
+	fmt.Println(expression)
 
 	if expression == "" {
-		c.JSON(400, gin.H{"Message": "请输入表达式expr"})
+		c.JSON(ExpressionIsEmpty, gin.H{Message: "请输入表达式expr"})
 		return
 	}
 
 	if match, _ := regexp.MatchString("[0-9]+|[\\+\\*/\\-]+", expression); match != true {
-		c.JSON(401, gin.H{"message": "有除了数字和字符串以外的字符"})
+		c.JSON(ThereAreOtherCharacters, gin.H{Message: "有除了数字和字符串以外的字符"})
 		return
 	}
 
 	expr, err := service.Calculate(expression)
 	if err != nil {
 		if err.Error() == "有连续操作符" {
-			c.JSON(402, gin.H{"Message": err.Error()})
+			c.JSON(ContinuousOperator, gin.H{Message: err.Error()})
 			return
 		} else if err.Error() == "最后一位必须为操作数" {
-			c.JSON(403, gin.H{"Message": err.Error()})
+			c.JSON(lastDigitIsNotNumber, gin.H{Message: err.Error()})
 			return
 		} else {
-			c.JSON(404, gin.H{"Message": err.Error()})
+			c.JSON(InvalidDivisor, gin.H{Message: err.Error()})
 			return
 		}
 
 	}
-	c.JSON(200, gin.H{"data": expr})
+	c.JSON(Success, gin.H{Message: expr})
+}
+
+func Controller2(c *gin.Context) {
+	exp := c.PostForm("exp")
+	if exp == "" {
+		c.JSON(ExpressionIsEmpty, gin.H{Message: StatusText(ExpressionIsEmpty)})
+		return
+	}
+	data, err := service.Caculator2(exp)
+
+	if err != nil {
+		c.JSON(FoundMistake, gin.H{Message: StatusText(FoundMistake)})
+		return
+	}
+	c.JSON(Success, gin.H{Message: StatusText(Success),
+		Data: data})
 }
--- model/model.go
+++ model/model.go
@@ -1,3 +1,32 @@
 package model
 
 type Stack []int
+
+type Stack1 []string
+
+//统计栈中元素的数量
+func (s *Stack1) Len() int {
+	return len(*s)
+}
+
+//将元素押入栈
+func (s *Stack1) Push(value string) {
+	*s = append(*s, value)
+}
+
+//获取栈顶元素
+func (s Stack1) Top() string {
+	return s[len(s)-1]
+}
+
+//弹出栈
+func (s *Stack1) Pop() string {
+	theStack := *s
+	value := theStack[len(theStack)-1]
+	*s = theStack[:len(theStack)-1]
+	return value
+}
+
+func (s *Stack1) Isempty() bool {
+	return len(*s) == 0
+}
