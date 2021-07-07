ecaa32264fea0f36fda2f362e10851bb485c8d00
89trillion-alimasi <alimasi@89trillion.com>
2021-07-07 22:54:05 +0800

    重新进行了单元测试

--- service/service.go
+++ service/service.go
@@ -3,11 +3,15 @@ package service
 import (
 	"CalculatorTool/model"
 	"errors"
+	"fmt"
 	"strconv"
+	"strings"
+	"unicode"
 )
 
 func Calculate(s string) (int, error) {
-
+	fmt.Printf(s)
+	s = strings.Replace(s, " ", "", -1)
 	stack := model.Stack{}
 	preSign := '+'
 	num := ""
@@ -20,7 +24,7 @@ func Calculate(s string) (int, error) {
 				return -1, errors.New("有连续操作符")
 			}
 		} else if i == len(s)-1 { //最后一位必须是操作数
-			if !lastisnum(s) {
+			if !Islastnum(s) {
 				return -1, errors.New("最后一位必须为操作数")
 			}
 		}
@@ -75,7 +79,115 @@ func checkDividend(num int) bool {
 }
 
 //判断最后一位是不是操作符
-func lastisnum(s string) bool {
+func Islastnum(s string) bool {
 	i := len(s) - 1
 	return '0' <= s[i] && s[i] <= '9'
+}
+
+func Caculator2(s string) (data int, err error) {
+	if !Judgement(s) {
+		return -1, errors.New("请输入正确的表达式")
+	}
+	suffix := MixTosuffix(s)
+
+	var curStack model.Stack1
+	for _, v := range suffix {
+		curChar := v
+		if i, ero := strconv.Atoi(v); ero == nil {
+			curStack.Push(strconv.Itoa(i))
+		} else {
+			v1 := curStack.Pop()
+			v2 := curStack.Pop()
+			num1, _ := strconv.Atoi(v1)
+			num2, _ := strconv.Atoi(v2)
+			switch curChar {
+			case "+":
+				curStack.Push(strconv.Itoa(num2 + num1))
+			case "-":
+				curStack.Push(strconv.Itoa(num2 - num1))
+			case "*":
+				curStack.Push(strconv.Itoa(num2 * num1))
+			case "/":
+				if num1 == 0 {
+					return -1, errors.New("除数不能为零")
+				}
+				curStack.Push(strconv.Itoa(num2 / num1))
+			}
+		}
+	}
+	ret := curStack.Top()
+	result, _ := strconv.Atoi(ret)
+	return result, nil
+}
+
+//中缀表达式转后缀表达式
+func MixTosuffix(exp string) []string {
+	var curStack model.Stack1
+	var suffix []string
+	expLen := len(exp)
+	for i := 0; i < expLen; i++ {
+		char := string(exp[i])
+		if char == " " {
+			continue
+		} else if unicode.IsDigit(rune(exp[i])) {
+			j := i
+			digit := ""
+			for ; j < expLen && unicode.IsDigit(rune(exp[j])); j++ {
+				digit += string(exp[j])
+			}
+			suffix = append(suffix, digit)
+			i = j - 1
+		} else {
+			for !curStack.Isempty() {
+				top := curStack.Top()
+				if priority(top, char) {
+					break
+				}
+				suffix = append(suffix, top)
+				curStack.Pop()
+			}
+			curStack.Push(char)
+		}
+	}
+	for !curStack.Isempty() {
+		data := curStack.Pop()
+		suffix = append(suffix, data)
+	}
+	return suffix
+}
+
+//判断优先级
+func priority(top, char string) bool {
+	switch top {
+	case "+", "-":
+		if char == "*" || char == "/" {
+			return true
+		}
+	}
+	return false
+}
+
+//判断表达式是否正确
+func Judgement(exp string) bool {
+	//如果算数表达式的第一个位置的元素不是数字或者最后一个位置的元素不是数字返回false
+	if !unicode.IsDigit(rune(exp[0])) || !unicode.IsDigit(rune(exp[len(exp)-1])) {
+		return false
+	}
+	flag := false
+	for _, char := range exp {
+		//如果字符为空，跳过判断
+		if char == ' ' {
+			continue
+		} else if !unicode.IsDigit(rune(char)) && flag == false {
+			//如果当前不是数字那么检查下一个是否是操作符
+			flag = true
+		} else if !unicode.IsDigit(rune(char)) && flag == true {
+			//不能是连续操作符
+			return false
+		} else if unicode.IsDigit(rune(char)) && flag == true {
+			//如果当前是数字，看下面是否还有数字和判断下个是否是操作符
+			flag = false
+		}
+	}
+	return true
 }
--- service/service_test.go
+++ service/service_test.go
@@ -24,3 +24,23 @@ func TestCalculate(t *testing.T) {
 	}
 
 }
+
+func TestCaculator2(t *testing.T) {
+	type tests struct {
+		exp  string
+		want int
+	}
+	test := []tests{
+		{exp: "1+2+3", want: 5},
+		{exp: "3+2-1+6/2", want: 7},
+		{exp: "3+2-1+6/0", want: -1},
+	}
+
+	for _, v := range test {
+		got, _ := Caculator2(v.exp)
+		if !reflect.DeepEqual(got, v.want) {
+			t.Error("expect:%v,got:%", v.want, got)
+		}
+	}
+
+}
